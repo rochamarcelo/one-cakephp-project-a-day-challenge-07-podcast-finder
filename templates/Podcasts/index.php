aa254fe032b8c0798299e186c698ef50efee5fdd
Marcelo Rocha <contato@omarcelo.com.br>
2021-07-18 17:43:24 -0300

    Style finder page with Bulma CSS Framework

--- templates/Podcasts/index.php
+++ templates/Podcasts/index.php
@@ -4,28 +4,56 @@
  * @var array $podcastData
  */
 ?>
-<?= $this->Form->create(null, ['type' => 'get'])?>
-<?= $this->Form->control('term')?>
-<?= $this->Form->submit(__('Find Now'))?>
-<?= $this->Form->end()?>
-<?php foreach ($podcastData['results'] as $podcast):?>
-<div>
-    <table>
-        <tr>
-            <td><?= $this->Html->image($podcast['artwork_url_100'])?></td>
-            <td>
-                <strong><?= $this->Html->link(
-                        $podcast['collection_name'],
-                        [
-                            'action' => 'episodes',
-                            $podcast['id'],
-                        ]
-                    )?>
-                </strong><br/>
-                <small><?= h($podcast['artist_name'])?></small>
-            </td>
-        </tr>
-    </table>
-    <hr/>
-</div>
-<?php endforeach;?>
+<article class="panel is-info">
+    <p class="panel-heading">
+        <?= __('Podcast Finder')?>
+    </p>
+    <?= $this->Form->create(null, ['type' => 'get'])?>
+    <div class="panel-block">
+        <div class="field has-addons">
+            <div class="control">
+                <?= $this->Form->text('term', [
+                    'class' => 'input is-info',
+                    'placeholder' => __('Search'),
+                    'label' => false,
+                ])?>
+            </div>
+            <div class="control">
+                <?= $this->Form->submit(__('Find'), [
+                    'class' => 'button is-info',
+                ])?>
+            </div>
+        </div>
+    </div>
+    <?= $this->Form->end()?>
+    <hr />
+    <?php foreach ($podcastData['results'] as $podcast):
+        $url = $this->Url->build(
+            $podcast['collection_name'],
+            [
+                'action' => 'episodes',
+                $podcast['id'],
+            ]
+        );
+    ?>
+    <a class="panel-block" href="<?= $url?>">
+        <article class="media">
+            <figure class="media-left">
+                <p class="image is-128x128">
+                    <?= $this->Html->image($podcast['artwork_url_100'], [
+                        'style' => 'height:90%',
+                    ])?>
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong><?= h($podcast['collection_name'])?></strong><br/>
+                        <small><?= h($podcast['artist_name'])?></small>
+                    </p>
+                </div>
+            </div>
+        </article>
+    </a>
+    <?php endforeach;?>
+</article>
